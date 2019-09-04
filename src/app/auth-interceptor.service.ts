a34158ee46a59e80581c6fda8280370fa39ea1da
“Maxaplays” <rgmateo@hotmail.com>
2019-09-04 16:26:28 -0500

    CLOUDTAXIS-262 Making http requests

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,5 +1,5 @@
 import {HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {tap} from "rxjs/operators";
+import {tap} from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
